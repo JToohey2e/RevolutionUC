c9a286e7e65cb37773e56a44d676e0ab2bf2145c
JToohey2e <57293142+JToohey2e@users.noreply.github.com>
2020-02-16 01:34:50 -0500

    Update RevolutionUC.py

--- RevolutionUC.py
+++ RevolutionUC.py
@@ -1,5 +1,13 @@
 print("hello world.")
 ans=input("What is answer?")
 ans=int(ans)
+#Sample Function
+def sampleFunction():
+    print("true")
+
+
 if ans==2:
     print("True")
+    sampleFunction()
+
+
