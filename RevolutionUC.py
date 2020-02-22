5ec4d57905a334e3ab0ea63c9ba5048c89308e9e
TheGraham117 <61032399+TheGraham117@users.noreply.github.com>
2020-02-22 13:40:08 -0500

    test
    j

--- RevolutionUC.py
+++ RevolutionUC.py
@@ -6,9 +6,14 @@ def sampleFunction():
     print("tgfnmhfgfhgfhe")
 
 
+
 if ans==2:
     i=2
+<<<<<<< HEAD
     var=7
+=======
+    var=8
+>>>>>>> a4e4cb4b397b0d488ad3bb3477d9bdce80418af3
     for i in range(2):
         var+=i
         print(var)
